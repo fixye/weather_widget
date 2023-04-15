1c7a12cf31a642b4ce39bf270f0a2ca8860a30c7
fixye <fixy2k13@gmail.com>
2023-04-16 01:24:17 +0200

    added basic style and data feed

--- vite-project/main.js
+++ vite-project/main.js
@@ -4,6 +4,10 @@ import key from "./key";
 const url = `https://api.openweathermap.org/data/2.5/weather?lat=50.1424&lon=15.1188&appid=${key}`;
 const iconUrl = `https://openweathermap.org/img/wn/{icon}@2x.png`
 const zeroK = 273.15;
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+const date = new Date()
 
 async function getWeatherData(url) {
     const response = await fetch(url);
@@ -18,7 +22,7 @@ getWeatherData(url).then((data) => {
         main: { feels_like: feelsLikeTemp },
     } = data;
     const {
-        wind: { speed: windSpeed, deg: windDeg, gust: windGust },
+        wind: { speed: windSpeed, deg: windDeg, gust: windGusts },
     } = data;
     const {
         sys: { sunrise: sunrise, sunset: sunset },
@@ -27,9 +31,21 @@ getWeatherData(url).then((data) => {
     const currentWeather = data.weather[0].main;
     const actualTempCelc = (actualTemp - zeroK).toFixed(0);
     const feelsLikeTempCelc = (feelsLikeTemp - zeroK).toFixed(0)
-
+    feedComponent(document.querySelector('.date'), `${months[date.getMonth()]} ${date.getDate()}`)
+    feedComponent(document.querySelector('.day'), days[date.getDay()])
+    feedComponent(document.querySelector('.big-number'), `${actualTempCelc}`)
+    feedComponent(document.querySelector('#fl'), `${feelsLikeTempCelc}`)
+    document.querySelector('.icon').src = `https://openweathermap.org/img/wn/${currentWeatherIcon}@2x.png`
+    feedComponent(document.querySelector('.name'), `${name} CZ`)
+    document.querySelector('.arrow').style.cssText = `
+        transform-origin: 50% 50%;
+        transform: rotate(${90 + windDeg}deg) scale(.4);
+    }
+    `
+    feedComponent(document.querySelector('.wind-speed'), `${windSpeed} m/s`)
+    feedComponent(document.querySelector('.gusts-speed'), `${windGusts} m/s`)
 });
 
-function feedComponent(element, data) {
-    element.textContent = data;
+function feedComponent(element, content) {
+    element.textContent = content;
 }
