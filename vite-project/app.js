d9d9c86927d40ea43aeb52ef299d80ebc664d432
fixye <fixy2k13@gmail.com>
2023-04-17 02:15:18 +0200

    added search bar with query api call

---
+++ vite-project/app.js
@@ -0,0 +1,5 @@
+class App {
+    constructor() {
+        this.searchBar = document.querySelector('.search-bar')
+    }
+}
--- vite-project/main.js
+++ vite-project/main.js
@@ -2,12 +2,36 @@ import "./style.css";
 import key from "./key";
 
 const url = `https://api.openweathermap.org/data/2.5/weather?lat=50.1424&lon=15.1188&appid=${key}`;
-const iconUrl = `https://openweathermap.org/img/wn/{icon}@2x.png`
+const iconUrl = `https://openweathermap.org/img/wn/{icon}@2x.png`;
 const zeroK = 273.15;
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+let debounce = null;
+const searchBar = document.querySelector(".search-bar");
+const suggestionsList = document.querySelector(".suggestions");
 
-const date = new Date()
+const date = new Date();
 
 async function getWeatherData(url) {
     const response = await fetch(url);
@@ -16,6 +40,10 @@ async function getWeatherData(url) {
 }
 
 getWeatherData(url).then((data) => {
+    content(data);
+});
+
+function content(data) {
     const { name } = data;
     const {
         main: { temp: actualTemp },
@@ -27,25 +55,98 @@ getWeatherData(url).then((data) => {
     const {
         sys: { sunrise: sunrise, sunset: sunset },
     } = data;
-    const { weather: [{ icon: currentWeatherIcon }] } = data
+    const {
+        weather: [{ icon: currentWeatherIcon }],
+    } = data;
     const currentWeather = data.weather[0].main;
     const actualTempCelc = (actualTemp - zeroK).toFixed(0);
-    const feelsLikeTempCelc = (feelsLikeTemp - zeroK).toFixed(0)
-    feedComponent(document.querySelector('.date'), `${months[date.getMonth()]} ${date.getDate()}`)
-    feedComponent(document.querySelector('.day'), days[date.getDay()])
-    feedComponent(document.querySelector('.big-number'), `${actualTempCelc}`)
-    feedComponent(document.querySelector('#fl'), `${feelsLikeTempCelc}`)
-    document.querySelector('.icon').src = `https://openweathermap.org/img/wn/${currentWeatherIcon}@2x.png`
-    feedComponent(document.querySelector('.name'), `${name} CZ`)
-    document.querySelector('.arrow').style.cssText = `
+    const feelsLikeTempCelc = (feelsLikeTemp - zeroK).toFixed(0);
+    feedContent(
+        document.querySelector(".date"),
+        `${months[date.getMonth()]} ${date.getDate()}`
+    );
+    feedContent(document.querySelector(".day"), days[date.getDay()]);
+    feedContent(document.querySelector(".big-number"), `${actualTempCelc}`);
+    feedContent(document.querySelector("#fl"), `${feelsLikeTempCelc}`);
+    document.querySelector(
+        ".icon"
+    ).src = `https://openweathermap.org/img/wn/${currentWeatherIcon}@2x.png`;
+    feedContent(document.querySelector(".name"), `${name} CZ`);
+    document.querySelector(".arrow").style.cssText = `
         transform-origin: 50% 50%;
         transform: rotate(${90 + windDeg}deg) scale(.4);
     }
-    `
-    feedComponent(document.querySelector('.wind-speed'), `${windSpeed} m/s`)
-    feedComponent(document.querySelector('.gusts-speed'), `${windGusts} m/s`)
-});
+    `;
+    feedContent(document.querySelector(".wind-speed"), `${windSpeed} m/s`);
+    feedContent(document.querySelector(".gusts-speed"), `${windGusts} m/s`);
+}
 
-function feedComponent(element, content) {
+function feedContent(element, content) {
     element.textContent = content;
 }
+
+searchBar.addEventListener("input", function () {
+    const value = searchBar.value;
+    if (value) querySearch(value);
+});
+
+function querySearch(value) {
+    clearTimeout(debounce);
+
+    debounce = setTimeout(() => {
+        fetch(
+            `http://api.openweathermap.org/data/2.5/find?q=${value}&type=like&appid=${key}`
+        )
+            .then((response) => {
+                if (response.ok) return response.json();
+                else {
+                    throw new Error("Bad request");
+                }
+            })
+            .catch((error) => {
+                console.log(error.message);
+            })
+            .then((data) => {
+                if (data.list) {
+                    suggestionsList.innerHTML = "";
+                    data.list.forEach((item, event) => {
+                        const li = document.createElement("li");
+                        li.textContent = `${item.name}, ${item.sys.country}`;
+                        suggestionsList.appendChild(li);
+
+                        li.addEventListener("click", async () => {
+                            searchBar.value = "";
+                            suggestionsList.innerHTML = "";
+                            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${item.coord.lat}&lon=${item.coord.lon}&appid=${key}`;
+                            try {
+                                getWeatherData(url).then((data) => {
+                                    content(data);
+                                });
+                            } catch (error) {
+                                console.log(error.message);
+                            }
+                        });
+                    });
+                    updateListHeight(data.list.length);
+                } else {
+                    return;
+                }
+            });
+    }, 500);
+}
+
+function updateListHeight(suggestions) {
+    const liHeight = 25;
+    const maxSuggestions = 10;
+    const numSuggestions = Math.min(suggestions, maxSuggestions);
+    const listHeight = numSuggestions * liHeight;
+    suggestionsList.style.height = `${listHeight + 5}px`;
+
+    if (numSuggestions < maxSuggestions) {
+        suggestionsList.style.overflowY = "hidden";
+    } else {
+        suggestionsList.style.overflowY = "auto";
+    }
+}
+
+updateListHeight(5);
